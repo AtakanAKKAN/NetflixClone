96f13e3d7032e4dc5fa8e5f47e20a0605d7cf7cd
Atakan AKKAN <85180975+AtakanAKKAN@users.noreply.github.com>
2022-12-06 19:08:25 +0300

    Add files via upload

--- java.js
+++ java.js
@@ -1,10 +1,40 @@
 window.onscroll = function() {scrollFunction()};
 
+
 function scrollFunction() {
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
         document.getElementById("navbar").style.backgroundColor = "rgb(20,20,20)";
+        document.getElementById("responsive-list").style.backgroundColor = "var(--colorOne)";
     }
     else {
         document.getElementById("navbar").style.backgroundColor = "transparent";
+        document.getElementById("responsive-list").style.backgroundColor = "transparent";
     }
 }
+
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+    spaceBetween: 5,
+    slidesPerView: 2,
+    freeMode: true,
+    loopAdditionalSlides: 5,
+    speed: 500,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    breakpoints:{
+      //when window width is >= 640px
+      640:{
+          slidesPerView: 5,
+          slidesPerGroup: 5,
+          freeMode: false,
+      },
+    },
+  
+  });
